ad3032e4d2ba3af1eb51c75f2431cf863c36ed26
Oruç Han Kural <77747739+oruchankural@users.noreply.github.com>
2022-07-03 16:02:49 +0300

    Add project files.

---
+++ Explicit/ExplicityDay.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explicit
+{
+    public struct ExplicityDay
+    {
+        public  int Day;
+
+        public ExplicityDay(int day)
+        {
+            if (day > 6)
+                day = 6;
+
+            Day = day;
+            DayOfWeek = (DayOfWeek)day;
+        }
+
+        public DayOfWeek DayOfWeek { get;}
+
+        public bool IsWeekDay => Day >= 1 && Day <= 5;
+
+        public bool IsWeekendDay => Day == 0 || Day == 6;
+
+        //public static implicit operator int(ExplicityDay day) => day.Day;
+
+        //public static implicit operator ExplicityDay(int day) => new ExplicityDay(day);
+
+
+        public static explicit operator int(ExplicityDay day) => day.Day; // if there is a conscious transformation
+
+        public static explicit operator ExplicityDay(int day) => new ExplicityDay(day); // if there is a conscious transformation
+
+    }
+}
---
+++ Explicit/Program.cs
@@ -0,0 +1,13 @@
+﻿using Explicit;
+using System;
+
+int dayNumber = 12;
+
+
+ExplicityDay newDay = new ExplicityDay(dayNumber);
+
+//int day = (int)newDay; // will be set day as int
+ExplicityDay asDay = (ExplicityDay)dayNumber; // will be set day as string
+
+Console.WriteLine(newDay.DayOfWeek);
+ 
